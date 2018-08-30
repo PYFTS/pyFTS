1ebe9e1249be297d786d5deb0ea97eb061f23baf
Petrônio Cândido <petronio.candido@gmail.com>
2018-08-30 17:08:27 -0300

    Updating the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ html_logo = 'logo_heading2.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
