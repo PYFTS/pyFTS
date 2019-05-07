ee17ea1e7d2a99f22503e7e398d0470ee5ffb823
Petrônio Cândido <petronio.candido@gmail.com>
2019-05-07 17:10:54 -0300

    Configuration and documentation update due the release 1.6

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pyFTS',
     packages=['pyFTS', 'pyFTS.benchmarks', 'pyFTS.common', 'pyFTS.data', 'pyFTS.models.ensemble',
               'pyFTS.models', 'pyFTS.models.seasonal', 'pyFTS.partitioners', 'pyFTS.probabilistic',
               'pyFTS.tests', 'pyFTS.models.nonstationary', 'pyFTS.models.multivariate',
-              'pyFTS.models.incremental', 'pyFTS.hyperparam', 'pyFTS.distributed', 'pyFTS.fcm'],
+              'pyFTS.models.incremental', 'pyFTS.hyperparam', 'pyFTS.distributed', 'pyFTS.fcm'],	
     version='1.6',
     description='Fuzzy Time Series for Python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Petronio Candido L. e Silva',
     author_email='petronio.candido@gmail.com',
     url='https://pyfts.github.io/pyFTS/',
@@ -18,5 +23,11 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Development Status :: 5 - Production/Stable'
+        
     ]
 )
