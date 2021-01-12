8bf3f7b1e9a4f6abc8de761cc18155a4a5d0173e
Petrônio Cândido <petronio.candido@gmail.com>
2021-01-12 20:10:49 -0300

    Documentation update

--- pyFTS/common/Transformations.py
+++ pyFTS/common/Transformations.py
@@ -5,15 +5,15 @@ Common data transformation used on pre and post processing of the FTS
 import numpy as np
 import pandas as pd
 import math
-#from pyFTS.common.transformations.transformation import Transformation
-#from pyFTS.common.transformations.differential import Differential
-#from pyFTS.common.transformations.scale import Scale
-#from pyFTS.common.transformations.adaptiveexpectation import AdaptiveExpectation 
-#from pyFTS.common.transformations.boxcox import BoxCox
-#from pyFTS.common.transformations.roi import ROI
-#from pyFTS.common.transformations.trend import LinearTrend
-#from pyFTS.common.transformations.som import SOMTransformation
-#from pyFTS.common.transformations.normalization import Normalization
+from pyFTS.common.transformations.transformation import Transformation
+from pyFTS.common.transformations.differential import Differential
+from pyFTS.common.transformations.scale import Scale
+from pyFTS.common.transformations.adaptiveexpectation import AdaptiveExpectation 
+from pyFTS.common.transformations.boxcox import BoxCox
+from pyFTS.common.transformations.roi import ROI
+from pyFTS.common.transformations.trend import LinearTrend
+from pyFTS.common.transformations.som import SOMTransformation
+from pyFTS.common.transformations.normalization import Normalization
 
 
 
